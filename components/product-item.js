dc4e45fa95e6ae3e75bd4c83c3f2e83b93f072ee
zvidmarb <50197931+zvidmarb@users.noreply.github.com>
2021-02-14 23:05:29 -0800

    Added attempted code :(

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,52 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor () {
+    super()
+    
+    //Create a shadow root
+    this.root = this.attachShadow({mode: 'open'})
+    
+    //Create custom list item
+    
+    const listItem = document.createElement("li")
+    this.setAttribute("class", "product")
+    root.appendChild(listItem)
+
+    let imgURL
+    if (this.hasAttribute("img")) {
+      imgURL = this.getAttribute("img")
+    }
+    const image = listItem.appendChild(document.createElement("img"))
+    image.setAttribute("src", imgUrl)
+    image.setAttribute("alt", title)
+
+    let title
+    if (this.hasAttribute("title")) {
+      title = this.getAttribute("title")
+    }
+    const titleHTML = listItem.appendChild(document.createElement("p"))
+    titleHTML.setAttribute("class", "title")
+    titleHTML.innerText = title
+
+    let price
+    if (this.hasAttribute("price")) {
+      price = this.getAttribute("price")
+    }
+    const priceHTML = listItem.appendChild(document.createElement("p"))
+    priceHTML.setAttribute("class", "price")
+    priceHTML.innerText = price
+
+    const button = listItem.appendChild(document.createElement("button"))
+    button.setAttribute("onclick", "alert('Added to Cart!')")
+    button.innerText = "Add to Cart"
+
+    
+  
+  }
+  connectedCallback() {
+    console.log(this)
+  }  
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,24 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  var jsonData  //data received from fetch
+  var jsonString  //fetch data retrieved from localStorage
+
+  //if the data hasn't been fetched yet
+  if (localStorage.getItem("jsonArray") == null) {
+    fetchProducts()
+  }
+  jsonString = localStorage.getItem("jsonArray")
+  jsonString = JSON.parse(jsonString)
+  console.log(jsonString)
+  
+  document.createElement("product-item.js")
+
 });
+
+async function fetchProducts() {
+  let response = await fetch("https://fakestoreapi.com/products")
+  jsonData = await response.json()
+  //console.log(jsonObject)
+  localStorage.setItem("jsonArray", JSON.stringify(jsonData))
+}
